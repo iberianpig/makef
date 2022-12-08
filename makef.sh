117344403e04e425b16944a40bfa7596b758ca25
iberianpig <yhkyky@gmail.com>
2022-12-08 16:58:16 +0900

    feat(*): generate .PHONY dynamically
    ref: https://qiita.com/buzztaiki/items/42885e32057c69ff8294

--- makef.sh
+++ makef.sh
@@ -64,5 +64,12 @@ all: help
 
 help: ## show this messages
 	@grep -E '^[a-zA-Z0-9_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+NO_PHONY = /^:/
+PHONY := $(shell cat $(MAKEFILE_LIST) | awk -F':' '/^[a-z0-9_.-]+:/ && !$(NO_PHONY) {print $$1}')
+.PHONY: $(PHONY)
+
+show_phony:
+	@echo $(PHONY)
 EOF
 }
